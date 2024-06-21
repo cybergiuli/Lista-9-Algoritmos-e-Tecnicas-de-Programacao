0119ddf94ed92d3404b41ceb6cc98b77319cfff2
Ana Giulia Abreu <166351955+cybergiuli@users.noreply.github.com>
2024-06-21 09:37:51 -0300

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    internal class Cliente
+    {
+		private string nome;
+		private string endere;
+		private string tel;
+
+		public Cliente (string nome, string endere, string tel)
+		{
+			this.nome = nome;
+			this.endere = endere;
+			this.tel = tel;
+		}
+
+        public Cliente(string nome, string endere)
+        {
+            this.nome = nome;
+            this.endere = endere;
+            tel = "0";
+        }
+
+        public string Nome
+        {
+            get { return Nome; }
+            set { Nome = value; }
+        }
+        public string Endere
+		{
+			get { return Endere; }
+			set { Endere = value; }
+		}
+
+        public string Tel
+        {
+            get { return Tel; }
+            set { Tel = value; }
+        }
+
+        public override string ToString()
+        {
+            return ($"Nome: {nome}, Endereço: {endere}, Telefone: {tel}");
+        }
+
+    }
+}
---
+++ Loja.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    internal class Loja
+    {
+
+        static void ImprimirClientes(Cliente[] clientes)
+        {
+            for (int i = 0; i < clientes.Length; i++)
+            {
+                Console.WriteLine(clientes[i]);
+            }
+        }
+
+        static void LerClientes(Cliente[] clientes)
+        {
+            string nome, ende, op, tel;
+
+            for (int i = 0; i < clientes.Length; i++)
+            {
+                Console.WriteLine("Escreva o seu nome:");
+                nome = Console.ReadLine();
+
+                Console.WriteLine("Escreva seu endereço:");
+                ende = Console.ReadLine();
+
+                Console.WriteLine("Você possi número de telefone? Digite sim ou não");
+                op = Console.ReadLine();
+
+                if (op == "sim")
+                {
+                    Console.WriteLine("Escreva seu telefone:");
+                    tel = Console.ReadLine();
+                    clientes[i] = new Cliente(nome, ende, tel);
+                }
+                else
+                {
+                    clientes[i] = new Cliente(nome, ende);
+                }
+
+                
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cliente[] clientes = new Cliente[3];
+            LerClientes(clientes);
+            ImprimirClientes(clientes);
+            Console.Read();
+        }
+    }
+}
